b9bed4a0e22dfb8c84292aa2bc30b738d3cc0a6f
Mohamed Ben Naima <medbennaima2021@gmail.com>
2025-07-02 02:29:18 +0100

    Added temporary blog page

---
+++ js/blog.js
@@ -0,0 +1,7 @@
+// js/blog.js - Blog Page Specific Scripts
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Any future interactivity unique to the Blog page would go here.
+    // For example, filtering blog posts, infinite scroll, etc.
+    // The global scroll animation logic for .animate-on-scroll is handled by main.js
+});
